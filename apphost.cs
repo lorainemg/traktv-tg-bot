9c28b367e7574957119a203048b4dc6106e76ddf
Daniel Rivero <danielrivgw@gmail.com>
2026-03-28 21:47:41 -0400

    Add db health check

--- apphost.cs
+++ apphost.cs
@@ -3,7 +3,11 @@
 #:package Aspire.Hosting.PostgreSQL@13.2.0
 #:package CommunityToolkit.Aspire.Hosting.Golang@13.2.1-beta.532
 
+using Aspire.Hosting.Docker.Resources.ComposeNodes;
+using Aspire.Hosting.Docker.Resources.ServiceNodes;
 using Aspire.Hosting.Pipelines;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
 
 #pragma warning disable ASPIREPIPELINES001
@@ -27,7 +31,18 @@ var telegramChatId = builder.AddParameter("telegram-chat-id");
 var postgres = builder.AddPostgres("postgres", dbUser, dbPassword)
 	.WithImageTag("17-alpine")
 	.WithEnvironment("POSTGRES_DB", dbName)
-	.WithDataVolume();
+	.WithDataVolume()
+	.PublishAsDockerComposeService((resource, service) =>
+	{
+		service.Healthcheck = new Healthcheck
+		{
+			Test = ["CMD-SHELL", "pg_isready -U ${POSTGRES_USER} -d {dbName}"],
+			Interval = "2s",
+			Timeout = "5s",
+			Retries = 5,
+			StartPeriod = "2s"
+		};
+	});
 
 var database = postgres.AddDatabase("traktdb", databaseName: dbName);
 
@@ -43,7 +58,14 @@ var bot = builder.AddGolangApp("bot", ".", "./cmd/bot")
 		context.EnvironmentVariables["DATABASE_URL"] = ReferenceExpression.Create($"{dbUri}?sslmode=disable");
 	})
 	.WithContainerRegistry(registry)
-	.WaitFor(postgres);
+	.WaitFor(postgres)
+	.PublishAsDockerComposeService((resource, service) =>
+	{
+		service.DependsOn["postgres"] = new ServiceDependency
+		{
+			Condition = "service_healthy"
+		};
+	});
 
 builder.Pipeline.AddStep(
 	"push-and-prepare-env",
